7e34001dbbb12a2fb5cb2a5a356fb7855441ecdd
Shrinivas Narhare <narhareshrinivas170@gmail.com>
2025-07-12 04:19:04 +0530

    Hardcode production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: (process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000') + '/api/'
+    baseURL: 'https://course-platform-api.onrender.com/api/'
 });
 
 // Use an interceptor to add the auth token to every request
