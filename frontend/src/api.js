ca5dc52629ab292de4ae3685273b1234339e4644
Shri Reddy <narhareshrinivas170@gmail.com>
2025-07-12 12:48:49 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://course-platform-api-rard.onrender.com/api/'
+    baseURL: 'https://course-platform-api-rard.onrender.com/'
 });
 
 // Use an interceptor to add the auth token to every request
